0f8385be3f46794a61a25afd7a1f2d27b2dc4670
Schwaitz <justinschwaitzberg@gmail.com>
2021-04-14 18:39:21 -0400

    Changed way videos for talent are fetched

--- app.py
+++ app.py
@@ -7,16 +7,26 @@ import app_config as app_config
 
 app = Flask(__name__)
 
-app.config['TESTING'] = False
+debug = True
+
+app.config['TESTING'] = debug
 app.config['SECRET_KEY'] = app_config.SECRET_KEY
 app.url_map.strict_slashes = False
 
 host = app_config.api_host
 
-def get_talents():
+
+def get_talents_json():
     talents_file = open('static/talents.json', 'r', encoding='utf-8')
     talent_data = json.loads(talents_file.read())
     talents_file.close()
+    return talent_data
+
+
+def get_talents():
+    
+    talent_data = requests.get(host + '/talents').json()
+
     return talent_data
 
 
@@ -53,35 +63,34 @@ def index():
     return render_template('index.html', title="Rabbithole")
 
 
-
 @app.route('/channels', methods=['GET'])
 def channels():
-    params = {'format': 'json'}
-    r = requests.get(host + '/channels', params=params).json()
+    
+    r = requests.get(host + '/channels').json()
     count = len(r)
     return render_template('channels.html', channel_data=r, channel_count=count)
 
 
 @app.route('/channels/<id>', methods=['GET'])
 def channel(id):
-    params = {'format': 'json'}
-    r = requests.get(host + '/channels/' + id + '/', params=params).json()
+    
+    r = requests.get(host + '/channels/' + id + '/').json()
     return render_template('channel.html', channel_id=r['channel_id'], channel_title=r['channel_title'], channel_description=r['channel_description'], channel_thumbnail_url=r['channel_thumbnail_url'])
     # return r
 
 
 @app.route('/videos', methods=['GET'])
 def videos():
-    params = {'format': 'json'}
-    r = requests.get(host + '/videos', params=params).json()
+    
+    r = requests.get(host + '/videos').json()
     count = len(r)
     return render_template('videos.html', video_data=r, video_count=count)
 
 
 @app.route('/videos/<id>', methods=['GET'])
 def video(id):
-    params = {'format': 'json'}
-    r = requests.get(host + '/videos/' + id + '/', params=params).json()
+    
+    r = requests.get(host + '/videos/' + id + '/').json()
     player_fixed = unescape(r['video_player'])
     print(player_fixed)
     return render_template('video.html', video_id=r['video_id'], video_title=r['video_title'], video_description=r['video_description'],
@@ -95,7 +104,7 @@ def video(id):
 
 @app.route('/talents', methods=['GET'])
 def talents():
-    talent_data = get_talents()
+    talent_data = get_talents_json()
 
     count = len(talent_data)
     return render_template('talents.html', talent_data=talent_data, talent_count=count)
@@ -103,32 +112,14 @@ def talents():
 
 @app.route('/talents/<name>', methods=['GET'])
 def talent(name):
-    talents = get_talents()
-    match = {}
+    data = requests.get(host + '/talents/' + name).json()
 
-    found = False
-    for t in talents:
-        if str(name).lower() == t['name'] or str(name).lower() in t['aliases']:
-            match = t
-            found = True
-            break
-
-    if found:
-        params = {'format': 'json'}
-        r = requests.get(host + '/videos', params=params).json()
-
-        matched_videos = []
-        for v in r:
-            if match['name'] in v['video_title'].lower() or any(alias in v['video_title'].lower() for alias in match['aliases']):
-                matched_videos.append(v)
-
-        count = len(matched_videos)
-
-        return render_template('talent.html', name=match['full_name'], matched_videos=matched_videos, match_count=count)
+    if data != {"message": "talent does not exist", "status": "fail"}:
+        return render_template('talent.html', data=data)
 
     else:
         return render_template('404.html')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=False)
+    app.run(host='127.0.0.1', port=8080, debug=debug)
